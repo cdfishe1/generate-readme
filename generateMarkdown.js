6a0a0f72e94116f932e6079f5f2cb766f09cf510
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-30 11:28:51 -0500

    successfully saved title of project to markdown file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -11,10 +11,9 @@
 //function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = data => {
   return `# ${data.title}
-
-`;
+  `
 }
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require('generateMarkdown.js');
+const generateMarkdown = require('./generateMarkdown.js');
 const inquirer = require('inquirer');
 const fs = require('fs');
-const writeFile = fs.writeFile;
-
-// TODO: Create an array of questions for user input
-const questions = [
-{type: 'input',
-message: 'What is the title of the project?',
-name: 'title',}];
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const promptUser = () => {
-    return inquirer.prompt([questions])
-}
+    return inquirer.prompt([
+    {type: 'input',
+    message: 'What is the title of the project?',
+    name: 'title'}
+    ])
+};
 
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
@@ -20,7 +19,7 @@ const promptUser = () => {
 // TODO: Create a function to initialize app
 const init = () => {
     promptUser()
-      .then((answers) => writeFile('README.md', generateMarkdown(answers)))
+      .then((answers) => writeFileAsync('README.md', generateMarkdown(answers)))
       .then(() => console.log('Successfully wrote to README.md'))
       .catch((err) => console.error(err));
   };
