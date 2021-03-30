cc7851ec004fb88b53ac378fa206156fcf407812
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-30 11:05:09 -0500

    set up for first question

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown = require('generateMarkdown.js');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const writeFile = fs.writeFile;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{type: 'input',
+message: 'What is the title of the project?',
+name: 'title',}];
+
+const promptUser = () => {
+    return inquirer.prompt([questions])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    promptUser()
+      .then((answers) => writeFile('README.md', generateMarkdown(answers)))
+      .then(() => console.log('Successfully wrote to README.md'))
+      .catch((err) => console.error(err));
+  };
 
 // Function call to initialize app
 init();
