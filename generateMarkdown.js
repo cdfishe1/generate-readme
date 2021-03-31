da6889981436613d400181ea113a33b8b9a3cedf
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-31 10:00:11 -0500

    Add links for all license choices

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -17,6 +17,16 @@ const generateMarkdown = data => {
   switch(data.license) {
     case 'MIT': licenseLink = 'https://opensource.org/licenses/MIT';
     break;
+
+    case 'Apache': licenseLink = 'https://www.apache.org/licenses/LICENSE-2.0';
+    break;
+
+    case 'GPL': licenseLink = 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+    break;
+
+    case 'BSD3': licenseLink = 'https://opensource.org/licenses/BSD-3-Clause';
+    break;
+    
     default: licenseLink = 'This app has no license.'
   }
   
